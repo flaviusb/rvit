2e25e946bfb5f832af3b5e9a00be10acbc4d7122
Justin Marsh (:flaviusb) <justin.marsh@flaviusb.net>
2019-05-01 14:22:40 +1200

    Revert "Add in barchart renderer."
    This reverts commit 4447b54ce42c8c961cc0ea3c230017024f94dc31.

--- rvit/core/widgets/__init__.py
+++ rvit/core/widgets/__init__.py
@@ -1,5 +1,4 @@
 from . import array_renderer
-from . import barchart_renderer
 # from .array_renderer import ArrayRenderer
 # from .point_renderer import PointRenderer
 # from .skivy_controller import SkivySlider
