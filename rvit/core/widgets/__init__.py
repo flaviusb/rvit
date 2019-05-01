9864ebd96dac2ae9877d22b430c23f7fd7db6aa9
Justin Marsh (:flaviusb) <justin.marsh@flaviusb.net>
2019-05-01 14:22:40 +1200

    Revert "Mechanical conversion of point_renderer."
    This reverts commit 03a82763631ba1b86617ffce7ff35fc45ade7563.

--- rvit/core/widgets/__init__.py
+++ rvit/core/widgets/__init__.py
@@ -1,5 +1,6 @@
 from . import array_renderer
 from . import barchart_renderer
-from . import point_renderer
+# from .array_renderer import ArrayRenderer
+# from .point_renderer import PointRenderer
 from . import rvit_controller
 # from .sprite_renderer import SpriteRenderer
