7d2bf143bbdf9d89cfd589a61480cf4765a861a4
Justin Marsh (:flaviusb) <justin.marsh@flaviusb.net>
2019-05-01 14:22:40 +1200

    Revert "More skivy->rvit, plus adding rvit.kv"
    This reverts commit 143832e9db16ae568e5e3e644f75846bd777ec69.

--- rvit/core/__init__.py
+++ rvit/core/__init__.py
@@ -44,35 +44,35 @@ def loadFonts():
         LabelBase.register(**font)
 
 
-def activate(rvit_path=None):
+def activate(skivy_path=None):
     pass
     global path, pfile_path, pars, inspection_path
 
-    Logger.info('==== Activating Rvit ====')
+    Logger.info('==== Activating SKivy ====')
 
-    if rvit_path is None:
-        rvit_path = os.path.dirname(sys.argv[0])
-        rvit_path = os.path.join(rvit_path, '.rvit')
-        rvit_path = os.path.abspath(rvit_path)
+    if skivy_path is None:
+        skivy_path = os.path.dirname(sys.argv[0])
+        skivy_path = os.path.join(skivy_path, '.skivy')
+        skivy_path = os.path.abspath(skivy_path)
 
-    # if no rvit dir exists, create it
+    # if no skivy dir exists, create it
     try:
-        Logger.info('Creating directory (%s) for Rvit.' % (rvit_path))
-        os.makedirs(rvit_path)
+        Logger.info('Creating directory (%s) for SKivy.' % (skivy_path))
+        os.makedirs(skivy_path)
     except os.error:
         pass
 
-    path = rvit_path
+    path = skivy_path
     inspection_path = os.path.join(path, 'inspections')
 
     # if no parameter file exists, create it
-    pfile_path = os.path.join(rvit_path, 'parameters.p')
+    pfile_path = os.path.join(skivy_path, 'parameters.p')
     pars = shelve.open(pfile_path)
     Logger.info('Parameter file: %s' % (pfile_path))
 
     loadFonts()
     kv_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                'rvit.kv')
+                                'skivy.kv')
     Builder.load_file(kv_file_path)
 
     # if not os.path.isfile(pfile_path) :
